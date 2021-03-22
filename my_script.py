8dbf96691e276be7713fb97ea9cf441d8864e92f
mrr28 <mrr28@njit.edu>
2021-03-22 03:10:27 -0400

    Fix: add in apostrophe

--- my_script.py
+++ my_script.py
@@ -1,6 +1,6 @@
 from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
-encoder.sae('./datamatrix_test.png)
+encoder.sae('./datamatrix_test.png')
 print(encoder.get_ascii())
 print("Hello world!")
